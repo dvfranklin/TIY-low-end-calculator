fc90ade1008923257fb32e4a14aed94fa6055265
Dan <dvfranklin@gmail.com>
2016-04-13 17:04:34 -0400

    Calculator.java will only accept valid numbers and operators

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,68 +9,69 @@ public class Calculator {
         double firstDouble = 0;
         double secondDouble = 0;
         double answer = 0;
-        String operator;
+        String operator = "";
         boolean validFirstNum = false;
         boolean validOperator = false;
         boolean validSecondNum = false;
 
         // get first operator - will loop until a valid number was entered
 
+
         System.out.println("Please enter a number: ");
 
-        while(!validFirstNum) {
+        while (!validFirstNum) {
             if (input.hasNextDouble()) {
                 firstDouble = input.nextDouble();
                 validFirstNum = true;
+            } else {
+                input.nextLine();
+                System.out.println("Please enter a valid number: ");
             }
         }
 
 
         // get operator
-        do {
-            System.out.print("Please enter an operation (+ - * / ");
-            operator = input.nextLine();
-        } while(!(operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")));
 
-
-
-        // make sure user entered a valid operation
-
-        // get second operand
-        System.out.print("Please enter a second number: ");
-        String secondNumber = input.nextLine();
-
-        try {
-            secondDouble = Double.parseDouble(secondNumber);
-        } catch (NumberFormatException e){
-            System.out.println("Please provide a valid number: ");
+        while (!validOperator) {
+            System.out.println("Please enter an operation (+ - * /): ");
+            operator = input.next();
+            if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
+                validOperator = true;
+            }
         }
 
-        // if so, compute answer
 
-        if(operator.equals("+")){
+        // get second operand, will loop until number is valid
+
+        System.out.println("Please enter a second number: ");
+
+        while (!validSecondNum) {
+            if (input.hasNextDouble()) {
+                secondDouble = input.nextDouble();
+                validSecondNum = true;
+            } else {
+                input.nextLine();
+                System.out.println("Please enter a valid number: ");
+            }
+        }
+
+
+        // compute answer and display
+
+        if (operator.equals("+")) {
             answer = firstDouble + secondDouble;
-        } else if(operator.equals("-")){
+        } else if (operator.equals("-")) {
             answer = firstDouble - secondDouble;
-        } else if(operator.equals("*")){
+        } else if (operator.equals("*")) {
             answer = firstDouble * secondDouble;
-        } else if(operator.equals("/")){
+        } else if (operator.equals("/")) {
             answer = firstDouble / secondDouble;
-        } else{
+        } else {
             System.out.println("I'm sorry, that does not appear to be a valid operation.");
         }
 
 
-
-
-
-
-
-
-
-
         System.out.println("Your answer is: " + answer);
-
 
     }
 }
